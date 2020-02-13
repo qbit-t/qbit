4d215a9792a5eed2f54f5401a38357cf9bb6d7bb
szhelonkin <zhelonkin@gravio.net>
2020-02-13 16:21:13 +0300

    Methods for hash

--- tests/hash.cpp
+++ tests/hash.cpp
@@ -1,13 +1,26 @@
 #include "hash.h"
+#include "../block.h"
+#include "../hash.h"
 #include "../hash/cuckoo.h"
 
 using namespace qbit;
 using namespace qbit::tests;
 
 bool HashTest::execute() {
-	uint256 hash;
+	Block blk;
+	blk.bits_ = 0x207fffff;
 	std::set<uint32_t> cycle;
-	//bool result = FindCycle(hash, 27 /*edge bits*/, 42 /* proof size */, cycle);
-	printf("sycel size %d\n", cycle.size());
+	for(int i=0; i<10000; i++)
+	{
+		blk.nonce_ = i;
+		bool result = FindCycle(blk.hash(), 20 /*edge bits*/, 42 /* 42 proof size */, cycle);
+		HashWriter lStream(SER_GETHASH, PROTOCOL_VERSION);
+		std::vector<uint32_t> v(cycle.begin(), cycle.end());
+		lStream << v;
+		uint256 ch = lStream.GetHash();
+		if (result) break;
+	}
+	printf("cycle size %d\n", cycle.size());
+
 	return true;
 }
--- uint256.cpp
+++ uint256.cpp
@@ -62,6 +62,40 @@ void base_blob<BITS>::setHex(const char* psz)
 }
 
 template <unsigned int BITS>
+base_blob<BITS>& base_blob<BITS>::operator<<=(unsigned int shift)
+{
+    base_blob<BITS> a(*this);
+    for (int i = 0; i < WIDTH; i++)
+        data[i] = 0;
+    int k = shift / 8;
+    shift = shift % 8;
+    for (int i = 0; i < WIDTH; i++) {
+        if (i + k + 1 < WIDTH && shift != 0)
+            data[i + k + 1] |= (a.data[i] >> (8 - shift));
+        if (i + k < WIDTH)
+            data[i + k] |= (a.data[i] << shift);
+    }
+    return *this;
+}
+
+template <unsigned int BITS>
+base_blob<BITS>& base_blob<BITS>::operator>>=(unsigned int shift)
+{
+    base_blob<BITS> a(*this);
+    for (int i = 0; i < WIDTH; i++)
+        data[i] = 0;
+    int k = shift / 8;
+    shift = shift % 8;
+    for (int i = 0; i < WIDTH; i++) {
+        if (i - k - 1 >= 0 && shift != 0)
+            data[i - k - 1] |= (a.data[i] << (8 - shift));
+        if (i - k >= 0)
+            data[i - k] |= (a.data[i] >> shift);
+    }
+    return *this;
+}
+
+template <unsigned int BITS>
 void base_blob<BITS>::setHex(const std::string& str)
 {
     setHex(str.c_str());
@@ -99,3 +133,28 @@ template std::string base_blob<512>::toString() const;
 template void base_blob<512>::setHex(const char*);
 template void base_blob<512>::setHex(const std::string&);
 template void base_blob<512>::set(unsigned char*);
+
+uint256& uint256::SetCompact(uint32_t nCompact, bool *pfNegative, bool *pfOverflow)
+{
+    int nSize = nCompact >> 24;
+    uint32_t nWord = nCompact & 0x007fffff;
+    if (nSize <= 3) {
+        nWord >>= 8 * (3 - nSize);
+        *this = nWord;
+    } else {
+        *this = nWord;
+        *this <<= 8 * (nSize - 3);
+    }
+    if (pfNegative)
+        *pfNegative = nWord != 0 && (nCompact & 0x00800000) != 0;
+    if (pfOverflow)
+        *pfOverflow = nWord != 0 && ((nSize > 34) ||
+                                     (nWord > 0xff && nSize > 33) ||
+                                     (nWord > 0xffff && nSize > 32));
+    return *this;
+}
+
+uint32_t uint256::GetCompact(bool fNegative) const
+{
+    return 0;
+}
