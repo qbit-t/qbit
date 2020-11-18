2dfd1c731866b31b8633a1aa6bc3e4a24ad38684
demuskov <demuskov@gmail.com>
2020-11-18 12:12:34 +0300

    Refactoring

--- pow.cpp
+++ pow.cpp
@@ -1,11 +1,12 @@
 #include "pow.h"
+#include "itransactionstore.h"
 
 namespace qbit {
 
-uint32_t GetNextWorkRequired(ITransactionStorePtr store, BlockHeader &current) {
+uint32_t getNextWorkRequired(ITransactionStorePtr store, BlockPtr current) {
 	uint32_t bits_limit = 553713663;
 	BlockHeader lPrev, lPPrev;
-	if(store->blockHeader(current.prev(), lPrev)) {
+	if(store->blockHeader(current->prev(), lPrev)) {
 		if(store->blockHeader(lPrev.prev(), lPPrev)) {
 			uint64_t block_found_time = lPrev.time_ - lPPrev.time_;
 			uint64_t time_limit = 10;
@@ -14,18 +15,18 @@ uint32_t GetNextWorkRequired(ITransactionStorePtr store, BlockHeader &current) {
     		bool fOverflow;
 			arith_uint256 target;
 			target.SetCompact(lPrev.bits_, &fNegative, &fOverflow);
+
+			/*
 			std::cout << "Was work " << bits_limit << " prev bits " << lPrev.bits_ << " " << fNegative << " " << fOverflow << std::endl ;
 			std::cout << "Next work " << target.GetCompact() << std::endl;
 			std::cout << "block found time " << block_found_time << std::endl;
 			std::cout << "time limit " << time_limit << std::endl;
 			std::cout << "time shift " << time_shift << std::endl;
+			*/
 
-			if(time_shift < 0.5)
-			{
+			if(time_shift < 0.5) {
 				target /= 2;
-			}
-			else if(time_shift > 2)
-			{
+			} else if(time_shift > 2) {
 				target *= 2;
 			}
 
@@ -33,8 +34,10 @@ uint32_t GetNextWorkRequired(ITransactionStorePtr store, BlockHeader &current) {
 			//target /= time_limit;
 			//target *= time_shift;
 
+			/*
 			std::cout << "Was work " << bits_limit << " prev bits " << lPrev.bits_ << " " << fNegative << " " << fOverflow << std::endl ;
 			std::cout << "Next work " << target.GetCompact() << std::endl;
+			*/
 			return target.GetCompact();
 		}
 	}
