ec62ad8cff4195a077050693b9ad1bc7235634f9
demuskov <demuskov@gmail.com>
2020-11-26 20:16:02 +0300

    Fix mining

--- block.cpp
+++ block.cpp
@@ -10,7 +10,7 @@ using namespace qbit;
 
 uint256 BlockHeader::hash() {
 	HashWriter lStream(SER_GETHASH, PROTOCOL_VERSION);
-	serialize_hash<HashWriter>(lStream);
+	serializeHash<HashWriter>(lStream);
 	return lStream.GetHash();
 }
 
