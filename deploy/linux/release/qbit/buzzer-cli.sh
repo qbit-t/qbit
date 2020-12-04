050d37ffaeb6f64abcdd5b02bac8e33cfb4d916b
demuskov <demuskov@gmail.com>
2020-12-04 20:23:59 +0300

    Add logging category

--- deploy/linux/release/qbit/buzzer-cli.sh
+++ deploy/linux/release/qbit/buzzer-cli.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/qbit/lib
-./qbit-cli -home .buzzer-cli -peers 212.71.253.201:31415,23.239.7.252:31415,139.162.39.249:31415 -debug info,warn,error,wallet,store
+./qbit-cli -home .buzzer-cli -peers 212.71.253.201:31415,23.239.7.252:31415,139.162.39.249:31415 -debug info,warn,error,wallet,store,client
 
