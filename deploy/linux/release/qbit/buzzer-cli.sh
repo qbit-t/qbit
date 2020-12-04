d0afe4dc4877f438f62a512d87cc9750bf01c494
demuskov <demuskov@gmail.com>
2020-12-04 19:57:42 +0300

    Deployment config

--- deploy/linux/release/qbit/buzzer-cli.sh
+++ deploy/linux/release/qbit/buzzer-cli.sh
@@ -1,4 +1,3 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/qbit/lib
-./qbit-cli -home .buzzer-cli -peers 127.0.0.1:31415 -debug info,warn,error,wallet,store,client
-
+./qbit-cli -home .buzzer-cli -peers 212.71.253.201:31415,23.239.7.252:31415,139.162.39.249:31415 -debug info,warn,error,wallet,store
 
